6f61376ee4f628fed47ca5d1732b2ac3b0f2b7e4
jingleBaba <umahatara@gmail.com>
2023-02-05 10:01:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -16,10 +16,31 @@ const boltReceiver = new ExpressReceiver({
 const boltApp = new boltAppConstructor({token: process.env.BOT_ACCESS_KEY, receiver: boltReceiver});
 // Initializes your app with your bot token and signing secret
 
-boltApp.event('app_mention', async ({ event, context, client, say }) => {
-  console.log("client", client);
+// Post a message to a channel your app is in using ID and message text
+async function publishMessage(id, text) {
+  try {
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await boltApp.client.chat.postMessage({
+      // The token you used to initialize your app
+      token: "xoxb-your-token",
+      channel: id,
+      text: text
+      // You could also use a blocks[] array to send richer content
+    });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+boltApp.event('message.im', async ({ event,say }) => {
+  console.log("running");
+  console.log("userID", event.user);
   // say() sends a message to the channel where the event was triggered
-  await say(`hello world`);
+    await publishMessage(event.user, 'hello test')
 });
 
 app.listen(process.env.PORT, () => {
@@ -30,4 +51,8 @@ app.get('/', (req,res) => {
   res.send("Welcome to CodeBaba");
 })
 
+app.post('/', (req,res) => {
+  console.log(req.body);
+  res.send("Welcome to CodeBaba");
+})
 app.use('/slack/events', boltApp.receiver.router);
