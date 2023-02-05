793ab04f22c59411515d0003bc4b6f1b85942628
jingleBaba <umahatara@gmail.com>
2023-02-05 10:27:11 +0530

    removed extra say function

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const boltApp = new boltAppConstructor({token: process.env.BOT_ACCESS_KEY, recei
 
 boltApp.event('message', ({ event,say }) => {
   say("Processing your request this might take a min or 2");
-  say(event.text);
 });
 
 app.listen(process.env.PORT, () => {
