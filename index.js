ec59976f6ebbc4fda137abbf35ae396673e425d4
jingleBaba <umahatara@gmail.com>
2023-02-05 10:16:32 +0530

    Updated event

--- index.js
+++ index.js
@@ -16,31 +16,12 @@ const boltReceiver = new ExpressReceiver({
 const boltApp = new boltAppConstructor({token: process.env.BOT_ACCESS_KEY, receiver: boltReceiver});
 // Initializes your app with your bot token and signing secret
 
-// Post a message to a channel your app is in using ID and message text
-async function publishMessage(id, text) {
-  try {
-    // Call the chat.postMessage method using the built-in WebClient
-    const result = await boltApp.client.chat.postMessage({
-      // The token you used to initialize your app
-      token: "xoxb-your-token",
-      channel: id,
-      text: text
-      // You could also use a blocks[] array to send richer content
-    });
 
-    // Print result, which includes information about the message (like TS)
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
 
 boltApp.event('message', async ({ event,say }) => {
-  console.log("running");
-  console.log("userID", event.user);
   say("Processing your request this might take a min or 2");
-  await publishMessage(event.user, 'hello test')
+  say(event.text);
+ 
 });
 
 app.listen(process.env.PORT, () => {
