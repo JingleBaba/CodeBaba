db69e0ea7515215b8584b8a6d2801115d248e12a
jingleBaba <umahatara@gmail.com>
2023-02-05 15:36:26 +0530

    fresh start

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-import Bolt from "@slack/bolt";
-import Express from 'express';
-import dotenv from 'dotenv';
-dotenv.config()
+const { App } = require('@slack/bolt');
 
-const app = Express();
-const ExpressReceiver = Bolt.ExpressReceiver;
-const boltAppConstructor = Bolt.App;
-
-const boltReceiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  appToken: process.env.APP_ACCESS_SECRET,
+const app = new App({
+  token: process.env.BOT_TOKEN, 
+  appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
-  endpoints:'/'
-})
-const boltApp = new boltAppConstructor({token: process.env.BOT_ACCESS_KEY, receiver: boltReceiver});
-// Initializes your app with your bot token and signing secret
-
-
-
-boltApp.event('message', async({ event,say }) => {
-  await say(event.text);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at ${process.env.PORT}`);
-})
+(async () => {
+  await app.start();
+  console.log('⚡️ Bolt app started');
+})();
 
-app.get('/', (req,res) => {
-  res.send("Welcome to CodeBaba");
-})
-
-app.post('/', (req,res) => {
-  console.log(req.body);
-  res.send("Welcome to CodeBaba");
-})
-app.use('/slack/events', boltApp.receiver.router);
+// subscribe to 'app_mention' event in your App config
+// need app_mentions:read and chat:write scopes
+app.event('app_mention', async ({ event, context, client, say }) => {
+  try {
+    await say({"blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Thanks for the mention <@${event.user}>! Here's a button`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Button",
+            "emoji": true
+          },
+          "value": "click_me_123",
+          "action_id": "first_button"
+        }
+      }
+    ]});
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
