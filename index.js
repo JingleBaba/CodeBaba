c1582f89123de238d556032fe847085bc599995b
jingleBaba <umahatara@gmail.com>
2023-02-05 06:30:28 +0530

    additional setups

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import Bolt from "@slack/bolt";
-
-import dotenv from 'dotenv'
+import Express from 'express';
+import dotenv from 'dotenv';
 dotenv.config()
 
-const App = Bolt.App;
-// Initializes your app with your bot token and signing secret
-const app = new App({
-  token: process.env.BOT_ACCESS_KEY,
+const app = Express();
+const ExpressReceiver = Bolt.ExpressReceiver;
+const boltAppConstructor = Bolt.App;
+
+const boltReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
-  appToken: process.env.APP_ACCESS_SECRET 
-});
+  appToken: process.env.APP_ACCESS_SECRET,
+  endpoints:'/'
+})
+const boltApp = new boltAppConstructor({token: process.env.BOT_ACCESS_KEY, receiver: boltReceiver});
+console.log(boltApp);
+// Initializes your app with your bot token and signing secret
 
-app.message('CodeBaba', async ({ message, say }) => {
+boltApp.message('CodeBaba', async ({ message, say }) => {
   console.log("running");
   // say() sends a message to the channel where the event was triggered
   await say(`Hey there <@${message.user}>!`);
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+app.listen(process.env.PORT, () => {
+  console.log("hello world");
+})
 
-  console.log('⚡️ Bolt app is running!');
-})();
+app.use('/', (req,res) => {
+  res.send("Welcome to codebaba");
+})
+
+app.use('/slack/events', boltApp.receiver.router);
