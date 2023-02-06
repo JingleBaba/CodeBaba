b5d98bd64832f57cc985040d53effffb59b8ce88
jingleBaba <umahatara@gmail.com>
2023-02-07 00:34:17 +0530

    added formatting and emojis

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const getResult = require('./modules/codex');
 const app = new App({
   token: process.env.BOT_TOKEN, 
   signingSecret: process.env.SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.APP_ACCESS_SECRET
 });
 
 app.event('message', async ({ event,say }) => {
   try {
-    await say("please wait while we fetch the result...");
+    await say("Please wait while i get these answers from the divine world... :stuck_out_tongue_closed_eyes:");
     const data =  await getResult(event.text);
-    await say(data);
+    await say("*Here's what i have found* :grin: ");
+    if(data.includes('<code>')) {
+      data.replace('<code>', '`');
+      data.replace('</code>','`');
+    }
+    await say("```"+data+"```");
   }
   catch (error) {
     console.error(error);
